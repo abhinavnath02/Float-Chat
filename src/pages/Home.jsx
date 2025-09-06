df0e15e553d63daa2f45b9d07afe842b832d244a
Madhuram2901 <patilmadhuram16@gmail.com>
2025-09-06 21:58:21 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       <div>
         <div className='absolute inset-0 flex top-20 pl-[35%]'>
           <h1 className='text-red-500 text-4xl md:text-6xl font-bold tracking-wide drop-shadow-lg'>
-            FLOAT-ChatBot
+            FLOAT-Chat
           </h1>
         </div>
         <div className='absolute inset-0 flex top-100 pl-[35%]'>
