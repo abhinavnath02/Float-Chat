a4e591bd5b73a22d06ebdab3b8e72224ae72f63d
Madhuram2901 <patilmadhuram16@gmail.com>
2025-09-06 21:54:43 +0530

    Update Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,17 @@ const Home = () => {
   return (
     <div className='h-screen w-screen relative'>
       <Video />
-      <div className='absolute inset-0 flex top-20 pl-[35%]'>
-        <h1 className='text-white text-4xl md:text-6xl font-bold tracking-wide drop-shadow-lg'>
-          FLOAT-ChatBot
-        </h1>
+      <div>
+        <div className='absolute inset-0 flex top-20 pl-[35%]'>
+          <h1 className='text-red-500 text-4xl md:text-6xl font-bold tracking-wide drop-shadow-lg'>
+            FLOAT-ChatBot
+          </h1>
+        </div>
+        <div className='absolute inset-0 flex top-100 pl-[35%]'>
+          <h1 className='text-white text-4xl md:text-6xl font-bold tracking-wide drop-shadow-lg'>
+            FLOAT-ChatBot
+          </h1>
+        </div>
       </div>
     </div>
   );
